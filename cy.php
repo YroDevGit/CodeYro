451c85c76c157c47c454c61c8edd4da3c9c472ca
YroDevGit <tyronemalocon@gmail.com>
2024-07-10 07:39:42 +0800

    add run server command

---
+++ cy.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author Tyrone Limen Malocom
+ * CodeYro
+ */
+$command = 'php -S localhost:8000';
+echo "CodeYro by: Yro Lee Emz\nStarting CodeYro server at http://localhost:8000\n";
+passthru($command);
+?>
