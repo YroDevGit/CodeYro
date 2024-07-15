f4af2ac04f62cf022cff3952697695c87d4c8062
YroDevGit <tyronemalocon@gmail.com>
2024-07-15 11:27:28 +0800

    CodeYRO email send

--- Front_End/SystemData/Email_config.php
+++ Front_End/SystemData/Email_config.php
@@ -5,12 +5,12 @@
  */
  //Configuration here...
  $protocol = 'smtp';
- $smtp_host = 'smtp.gmail.com'; //email host
+ $smtp_host = 'smtp.gmail.com'; //gmail host//You can change it
  $smtp_port = 587; // or 465 for SSL
- $smtp_user = ""; //Your email address
- $smtp_pass = ""; //Your email password
+ $smtp_user_email = ""; //Your email address
+ $smtp_pass = ""; //Your email app password //Password should not have spaces
  $mail_type = "html"; 
- $charset = "iso-8859-1";
+ $charset = "utf-8";
 
 
 
@@ -58,7 +58,7 @@
  define("cy_protocol", $protocol);
  define('cy_smtp_host', $smtp_host);
  define('cy_smtp_port', $smtp_port);
- define('cy_smtp_user', $smtp_user);
+ define('cy_smtp_user', $smtp_user_email);
  define('cy_smtp_pass', $smtp_pass);
  define('cy_mail_type', $mail_type);
  define('cy_charset', $charset);
--- Front_End/system/helpers/cyemail_helper.php
+++ Front_End/system/helpers/cyemail_helper.php
@@ -2,13 +2,13 @@
 
 
  if(! function_exists("CY_SEND_EMAIL")){
-    function CY_SEND_EMAIL($sender_name, $from , $to, $subject="No Subject", $email_view="cy_email", $data = ["title" => "Welcome to CY mailer", "message" => "This is just a test codeyro mailer."]) {
-        /** => Integer
-         * 200 when success (CY_SUCCESS, CY_SUCCESS_CODE).
-         * -1 when fail sending email.
+    function CY_SEND_EMAIL($sender_name, $to, $subject="No Subject", $email_view="cy_email", $data = ["title" => "Welcome to CY mailer", "message" => "This is just a test codeyro mailer."]) {
+        /** => Array
+         * send email.
+         * Return result array
          */
         include_once "Front_End\SystemData\Email_config.php";
-        $ret = -1;
+        $ret = [];
         $CY =& get_instance();
     
         $config = array(
@@ -18,12 +18,17 @@
             'smtp_user' => cy_smtp_user,
             'smtp_pass' => cy_smtp_pass,
             'mailtype'  => cy_mail_type, 
-            'charset'   => cy_charset
+            'charset'   => cy_charset,
+            'smtp_crypto' => 'tls',
+            'wordwrap'  => TRUE,
+            'smtp_timeout' => 30,
+            'smtp_debug' => 2,
+            'newline'   => "\r\n",
         );
     
         $CY->email->initialize($config);
     
-        $CY->email->from($from, $sender_name);
+        $CY->email->from(cy_smtp_user, $sender_name);
         $CY->email->to($to);
         $CY->email->subject($subject);
     
@@ -32,9 +37,9 @@
         $CY->email->message($htmlContent);
     
         if ($CY->email->send()) {
-            $ret = CY_SUCCESS_CODE;
+            $ret = ["code"=>CY_SUCCESS, "message"=>"SUCCESS"];
         } else {
-            $ret = -1;
+            $ret = ["CODE"=>-1, "message"=>$CY->email->print_debugger()];
             //echo $CY->email->print_debugger();
         }
         return $ret;
